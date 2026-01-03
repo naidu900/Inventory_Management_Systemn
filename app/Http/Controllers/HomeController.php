9cf66eb456273a34ceb27b9afa35f4a1234d39a3
LEON <naidudusmanta@#gmail.com>
2026-01-03 16:20:59 +0530

    search and admin fixed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -11,4 +12,20 @@ class HomeController extends Controller
         $products = Product::latest()->get();
         return view('home', compact('products'));
     }
+
+public function filter(Request $request)
+{
+    $search = $request->query('search');
+
+    $products = Product::when($search, function ($query) use ($search) {
+        $query->where(function ($q) use ($search) {
+            $q->where('name', 'LIKE', "%{$search}%");
+        });
+    })
+    ->latest()
+    ->get();
+
+    return view('home', compact('products', 'search'));
+}
+
 }
